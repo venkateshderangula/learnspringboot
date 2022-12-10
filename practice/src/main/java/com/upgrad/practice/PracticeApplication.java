46193490ab7c9cfbcb71e25d0a5ac60b9020cf28
Venky <venkateshderangula8906@gmail.com>
2022-12-11 00:57:14 +0530

    First RestAPI

--- practice/src/main/java/com/upgrad/practice/PracticeApplication.java
+++ practice/src/main/java/com/upgrad/practice/PracticeApplication.java
@@ -2,12 +2,23 @@ package com.upgrad.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+		System.out.println("application running...");
+	}
+
+
+	@RequestMapping("/")
+	public String helloPeople(){
+		return "Hello, People! Welcome to Spring boot world. Spring boot is nothing but Spring on steroids";
 	}
 
 }
